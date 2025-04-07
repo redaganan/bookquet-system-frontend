9bedf62e2dd0f295b644ac7ec7f0d73cab7cf065
J1nWo0 <sherwinlaguidao533@gmail.com>
2025-04-07 16:55:07 +0800

    feat: update Body and BodyOfCollections components to include navigation links; remove unused search bar styles from home.css

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,33 +1,35 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/home.css";
 function Body() {
-  const navigate = useNavigate();
-  const handleClick = () => {
-	navigate("/collections");
-  };
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate("/collections");
+	};
 
-  return (
-    <>
-      <div className="container">
-        <h2>Fresh Flowers for Every Occasion</h2>
-        <p>Discover handcrafted bouquets and floral arrangements</p>
-        <button onClick={handleClick}>Shop Now</button>
-      </div>
-      <div className="products">
-        <div className="product">
-          <img src="bestseller.jpg" alt="Best Sellers" />
-          <p>
-            <strong>Best Sellers</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="bundles.jpg" alt="bundles" />
-          <p>
-            <strong>Bundles</strong>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container">
+				<h2>Fresh Flowers for Every Occasion</h2>
+				<p>Discover handcrafted bouquets and floral arrangements</p>
+				<button onClick={handleClick}>Shop Now</button>
+			</div>
+			<div className="products">
+				<div className="product">
+					<Link to="/collections">
+						<img src="bestseller.jpg" alt="Best Sellers" />
+						<p>
+							<strong>Best Sellers</strong>
+						</p>
+					</Link>
+				</div>
+				<div className="product">
+					<img src="bundles.jpg" alt="bundles" />
+					<p>
+						<strong>Bundles</strong>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
 export default Body;
--- src/components/BodyOfCollections.tsx
+++ src/components/BodyOfCollections.tsx
@@ -1,104 +1,104 @@
 import "../styles/home.css";
 function Body() {
-  return (
-    <>
-      <div className="collections-header">
-        <h1>Collections</h1>
-        <h3>For Anniversary </h3>
-      </div>
-      <div className="products">
-        <div className="product">
-          <img src="../flowers/anniversary/Cherished-Love-Bundle.png" />
-          <p>
-            <strong>Cherished-Love-Bundle</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\anniversary\Daffodil.jpg" />
-          <p>
-            <strong>Daffodil</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\anniversary\Lilac.webp" />
-          <p>
-            <strong>Lilac</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\anniversary\roses.jpg" />
-          <p>
-            <strong>roses</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\anniversary\Sunflower.jpg" />
-          <p>
-            <strong>Sunflowe</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\anniversary\White-roses.jpg" />
-          <p>
-            <strong>White-roses</strong>
-          </p>
-        </div>
-      </div>
-      <h3>For Birthday </h3>
-      <div className="products">
-        <div className="product">
-          <img src="public\flowers\birthday\amalia.webp" />
-          <p>
-            <strong>amalia</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\birthday\candy-rush.webp" />
-          <p>
-            <strong>candy-rush</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\birthday\Heather-Birthday-Bundle.webp" />
-          <p>
-            <strong>Heather Birthday Bundle</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\birthday\Stargazer.jpg" />
-          <p>
-            <strong>Stargazer</strong>
-          </p>
-        </div>
-      </div>
-      <h3>For Funeral </h3>
-      <div className="products">
-        <div className="product">
-          <img src="public\flowers\funeral\deepest-sympathies-funeral-flower-basket.webp" />
-          <p>
-            <strong>deepest sympathies</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\funeral\faith-and-adoration-sympathy-flower.webp" />
-          <p>
-            <strong>Faith and Adoration</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\funeral\rest-in-peace-funeral-condolences-flower-stand.webp" />
-          <p>
-            <strong>Peace flower</strong>
-          </p>
-        </div>
-        <div className="product">
-          <img src="public\flowers\funeral\white-condolence-flowers.webp" />
-          <p>
-            <strong>White Condolence</strong>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="collections-header">
+				<h1>Collections</h1>
+				<h3>For Anniversary </h3>
+			</div>
+			<div className="products">
+				<div className="product">
+					<img src="../flowers/anniversary/Cherished-Love-Bundle.png" />
+					<p>
+						<strong>Cherished-Love-Bundle</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\anniversary\Daffodil.jpg" />
+					<p>
+						<strong>Daffodil</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\anniversary\Lilac.webp" />
+					<p>
+						<strong>Lilac</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\anniversary\roses.jpg" />
+					<p>
+						<strong>roses</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\anniversary\Sunflower.jpg" />
+					<p>
+						<strong>Sunflowe</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\anniversary\White-roses.jpg" />
+					<p>
+						<strong>White-roses</strong>
+					</p>
+				</div>
+			</div>
+			<h3>For Birthday </h3>
+			<div className="products">
+				<div className="product">
+					<img src="..\flowers\birthday\amalia.webp" />
+					<p>
+						<strong>amalia</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\birthday\candy-rush.webp" />
+					<p>
+						<strong>candy-rush</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\birthday\Heather-Birthday-Bundle.webp" />
+					<p>
+						<strong>Heather Birthday Bundle</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\birthday\Stargazer.jpg" />
+					<p>
+						<strong>Stargazer</strong>
+					</p>
+				</div>
+			</div>
+			<h3>For Funeral </h3>
+			<div className="products">
+				<div className="product">
+					<img src="..\flowers\funeral\deepest-sympathies-funeral-flower-basket.webp" />
+					<p>
+						<strong>deepest sympathies</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\funeral\faith-and-adoration-sympathy-flower.webp" />
+					<p>
+						<strong>Faith and Adoration</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\funeral\rest-in-peace-funeral-condolences-flower-stand.webp" />
+					<p>
+						<strong>Peace flower</strong>
+					</p>
+				</div>
+				<div className="product">
+					<img src="..\flowers\funeral\white-condolence-flowers.webp" />
+					<p>
+						<strong>White Condolence</strong>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
 export default Body;
